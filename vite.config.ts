6965ddfde620ef94d6b4c2aa7d57f5a02d80ec0b
AlexsanderSouza <alexsander.rafael2022@gmail.com>
2025-03-11 21:07:58 -0400

    configure Vite to exclude TypeScript icons from simple-icons in the build

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['simple-icons', 'react-icon-cloud']
+      external: ['simple-icons/icons/typescript']
     }
   },
 });
