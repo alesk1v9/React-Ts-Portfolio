858ced8d413f8f5cb2da9603a5bde9e23adf65a8
AlexsanderSouza <alexsander.rafael2022@gmail.com>
2025-03-11 21:01:59 -0400

    configure Vite to exclude react-icon-cloud from the build

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['simple-icons']
+      external: ['simple-icons', 'react-icon-cloud']
     }
   },
 });
